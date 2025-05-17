b75d6d1d1ce42dd46f5a82a16ea12d407662bc64
l.avelino@uniandes.edu.co <l.avelino@uniandes.edu.co>
2025-05-17 15:03:53 -0500

    Test completos del combo

--- Taller 5 Hamburguesas/tests/uniandes/dpoo/hamburguesas/tests/ComboTest.java
+++ Taller 5 Hamburguesas/tests/uniandes/dpoo/hamburguesas/tests/ComboTest.java
@@ -2,13 +2,60 @@ package uniandes.dpoo.hamburguesas.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ComboTest {
+import uniandes.dpoo.hamburguesas.mundo.Combo;
+import uniandes.dpoo.hamburguesas.mundo.*;
+import java.util.*;
 
+class ComboTest {
+	private ProductoMenu ing1;
+	private ProductoMenu ing2;
+	private ProductoMenu ing3;
+	private Combo com;
+	
+
+    @BeforeEach
+    public void setUp() {
+    	ProductoMenu ing1 = new ProductoMenu("Hmburguesa Sencilla",1000);
+		ProductoMenu ing2 = new ProductoMenu("Papas Medianas",500);
+		ProductoMenu ing3 = new ProductoMenu("Gaseosa",700);
+		
+		ArrayList<ProductoMenu> lista = new ArrayList<>();
+		lista.add(ing3);
+		lista.add(ing2);
+		lista.add(ing1);
+ 		
+		com = new Combo("Especial",0.07,lista);
+		  
+    }
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void generarFactura() {
+	String nom = com.getNombre();
+	StringBuffer sb = new StringBuffer( );
+    sb.append( "Combo " + nom + "\n" );
+    sb.append( " Descuento: " + "0.07" + "\n" );
+    sb.append( "            " + "154" + "\n" );
+    String esperado = sb.toString();
+	String resp = com.generarTextoFactura();
+	
+	assertEquals(resp,esperado,	"No esta dando bien el texto de la factura");
+	
+		
+		
 	}
 
+	
+	@Test
+	void Precio() {
+		assertEquals(154,com.getPrecio(),"No calcula bien el descuento");
+	}
+	
+	
+	@Test
+	void getNombre() {
+		assertEquals("Especial",com.getNombre(),"No devuelve el nombre como debe ser");
+	}
 }
